3d42ca331282f8f8bf25d45f96ebcc2bbb91d91d
ArthurWidler <arthur.widler@lernende.bbw.ch>
2023-01-19 21:55:18 +0100

    Initial commit

---
+++ src/test/java/com/example/m165_quiz/M165QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m165_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M165QuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
